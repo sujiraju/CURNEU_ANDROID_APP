1a73cbc651deeecd4bd943e916c5cfe279b8d4e7
sujiraju <sujiraju16@gmail.com>
2018-09-28 10:30:03 +0530

    initial commit

---
+++ app/src/main/java/com/example/priyankamoorthy/tickettask/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.priyankamoorthy.tickettask;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import static android.content.ContentValues.TAG;
+
+public class MainActivity extends Activity {
+
+    private EditText et_name,et_email;
+    private Button bt_login;
+    private DatabaseReference mFirebaseDatabase;
+    private FirebaseDatabase mFirebaseInstance;
+    private String userId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    }
+
---
+++ app/src/main/java/com/example/priyankamoorthy/tickettask/User.java
@@ -0,0 +1,13 @@
+package com.example.priyankamoorthy.tickettask;
+
+public class User {
+    public String name;
+    public String email;
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
