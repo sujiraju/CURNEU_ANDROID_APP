6583b5bab8e60d1b0267e275b658cd05982cd35d
sujiraju <sujiraju16@gmail.com>
2018-09-28 12:40:02 +0530

    user added to db

--- app/src/main/java/com/example/priyankamoorthy/tickettask/MainActivity.java
+++ app/src/main/java/com/example/priyankamoorthy/tickettask/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
@@ -21,15 +22,48 @@ public class MainActivity extends Activity {
 
     private EditText et_name,et_email;
     private Button bt_login;
-    private DatabaseReference mFirebaseDatabase;
-    private FirebaseDatabase mFirebaseInstance;
+    private DatabaseReference databaseUsers;
     private String userId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        databaseUsers = FirebaseDatabase.getInstance().getReference("user");
+
+        et_name = (EditText) findViewById(R.id.name);
+        et_email = (EditText) findViewById(R.id.email);
+        bt_login = (Button) findViewById(R.id.login);
+
+        bt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addUser();
+            }
+        });
     }
+
+    public void addUser(){
+        String name = et_name.getText().toString().trim();
+        String email  = et_email.getText().toString().trim();
+
+        if(!TextUtils.isEmpty(name) && !TextUtils.isEmpty(email)){
+
+           userId =  databaseUsers.push().getKey();
+
+           User user = new User(userId,name,email);
+
+           databaseUsers.child(userId).setValue(user);
+
+            Toast.makeText(this, "User added", Toast.LENGTH_LONG).show();
+
+        }else{
+            Toast.makeText(this, "Enter valid name and email", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
     }
 
--- app/src/main/java/com/example/priyankamoorthy/tickettask/User.java
+++ app/src/main/java/com/example/priyankamoorthy/tickettask/User.java
@@ -1,13 +1,33 @@
 package com.example.priyankamoorthy.tickettask;
 
 public class User {
+
+    public String usrid;
     public String name;
     public String email;
+
+
     public User() {
     }
 
-    public User(String name, String email) {
+    public User(String usrid, String name, String email) {
+        this.usrid = usrid;
         this.name = name;
         this.email = email;
     }
+
+    public String getUsrid() {
+        return usrid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+
 }
